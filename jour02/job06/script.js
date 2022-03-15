ec04b322453471a7f601a7cd19556ee0da2b7068
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-03-15 16:20:43 +0100

    double click

---
+++ jour02/job06/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function loaded() {
+
+    var div = document.querySelectorAll('div');
+    
+    window.addEventListener('keydown', function (event){
+    let key = event.key;
+
+        if(event.key = "ArrowUp")
+        {
+            if(event.key = "ArrowUp"){
+                console.log('ok');
+            } 
+        }
+    })
+
+
+})
