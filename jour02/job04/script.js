db377588ae52a7657afd76abfb0b5396c5a97818
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-03-15 11:58:45 +0100

    job04 entamé

---
+++ jour02/job04/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function loaded() {
+
+    let test = window.addEventListener('keydown', function (){
+        var ok = hey.key;
+        console.log(ok);
+    })
+
+    let textarea = document.getElementById('keylogger');
+    console.log(textarea);
+})
