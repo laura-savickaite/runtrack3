8011c945cd358c41c176548067c88ff57dace6e0
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-03-15 11:04:54 +0100

    job01 done

--- jour02/job01/script.js
+++ jour02/job01/script.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function loaded() {
-    document.getElementById()
+    let button = document.getElementById('button');
 
-
+    button.addEventListener('click', function citation(){
+        let citation = document.getElementById('citation');
+        let string = citation.querySelector('p');
+        console.log(string);
+    })  
 })
