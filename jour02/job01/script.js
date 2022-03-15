1b72b314c4f79c7949ed6acd7c5799761ee405ee
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-03-15 11:17:54 +0100

    ajout des fichiers de jour01

--- jour02/job01/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function loaded() {
-    let button = document.getElementById('button');
-
-    button.addEventListener('click', function citation(){
-        let citation = document.getElementById('citation');
-        let string = citation.querySelector('p');
-        console.log(string);
-    })  
-})
