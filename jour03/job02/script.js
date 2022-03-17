1b890ebc86d7c48995dde59d5d4458b1eb695f7d
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-03-17 11:47:56 +0100

    job01 fait

--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -2,14 +2,35 @@ $(document).ready(function(){
 
     var button = $('#button');
     var images = $('img');
-    console.log(images);
+    // console.log(images);
+    const array = ['images/arc1.png', 'images/arc2.png', 'images/arc3.png', 'images/arc4.png', 'images/arc5.png', 'images/arc6.png'];
 
-    var hey = $.map(images);
-    console.log(hey);
+    
 
-    $(button).click(function(){
-        console.log(images)
-        let img = $(this);
-        console.log(img);
-    })
+    // $(button).click(function(){
+    //     $('#melangee').rdmGrid();
+    // })
+
+    // function shuffleArray(array) {
+    //     // console.log(array.length);
+    //     for (var i = array.length-1; i > 0; i--) {
+    //         var j = Math.floor(Math.random() * (i + 1));
+    //         var temp = array[i];
+    //         array[i] = array[j];
+    //         array[j] = temp;
+    //         images.src = array[i];
+    //     }
+    // }
+
+    shuffleArray(array);
+    // images.click(function(){
+    //     let valeur = $(this).attr('src') 
+    //     range.push(valeur);
+    //     console.log(range)
+    //     //et j'envoie dans la div rangée
+    // })
+
+
+
+
 })
--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -4,17 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="script.js" charset="utf-8"></script>
     <title>runtrack3</title>
 </head>
 <body>
-    <button id="button">Mélanger</button>
-    <div id='melangee'>
-        <img src=''>
-    </div>
-    <div id='rangee'>
-
-    </div>
+    <p></p>
+    <button>Afficher</button>
 </body>
 </html>
