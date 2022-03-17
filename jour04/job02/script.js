0c28274491938c262ca163313e5399121b60ed61
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-03-17 14:26:28 +0100

    job02 done je crois (?)

---
+++ jour04/job02/script.js
@@ -0,0 +1,19 @@
+// Créez une fonction javascript “jsonValueKey()” qui prend en paramètre une
+// chaîne de caractères au format json et une clé. Cette fonction retourne la
+// valeur liée à cette clé dans la chaîne de caractères. Par exemple si la
+// string passée en paramètre est “{name: "La Plateforme_", address: "8 rue
+// d'hozier", city: "Marseille", nb_staff: "11", creation:"2019"}” et la clé est
+// “city”, la fonction retourne “Marseille”.
+
+document.addEventListener('DOMContentLoaded', function loaded() {
+
+    function jsonValueKey(string, key){
+
+        let test = JSON.parse(string)
+        // console.log(test);
+
+        let ok = test[key]
+    }
+
+    jsonValueKey('{"name": "La Plateforme_", "address": "8 rue dhozier", "city": "Marseille", "nb_staff": 11, "creation": 2019 }', 'name');
+})
