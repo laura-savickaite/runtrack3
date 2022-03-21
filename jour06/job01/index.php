962443cbc4990c390db8e13c099bc46ac8d9308b
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-03-21 11:12:06 +0100

    job 01 fini

---
+++ jour06/job01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style1.css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 1920px)" href="style2.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 1680px)" href="style3.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 1280px)" href="style4.css"/>
+    <title>runtrack3</title>
+</head>
+<body>
+    
+</body>
+</html>
